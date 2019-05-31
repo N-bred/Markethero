838b730756c6bdd89f00519c757b10b48d54adae
Nicolas Caballero <37151000+N-bred@users.noreply.github.com>
2019-05-31 10:36:47 -0500

    Fixed some problems with responsive layout

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 (function() {
-  // Var declaration
-  const header = document.querySelector('.header'),
-    sidebar = document.querySelector('.sidebar__logo'),
-    col_1 = document.querySelector('.col-1'),
-    button = document.querySelector('.header__menu-button'),
-    yearSpan = document.querySelector('#footer_year'),
-    date = new Date();
+   // Var declaration
+   const header = document.querySelector('.header'),
+      sidebar = document.querySelector('.sidebar__logo'),
+      col_1 = document.querySelector('.col-1'),
+      button = document.querySelector('.header__menu-button'),
+      yearSpan = document.querySelector('#footer_year'),
+      date = new Date();
 
-  // Height fix to nav
-  header.style.height = String(sidebar.offsetHeight) + 'px';
+   // Height fix to nav
 
-  // Button for sidebar slide
-  button.addEventListener('click', () => {
-    if (window.innerWidth > 990) {
-      col_1.classList.toggle('sidebar--toggle');
-    } else if (window.innerWidth < 990) {
-      col_1.classList.toggle('sidebar--toggle-inverse');
-    }
-  });
+   header.style.height = String(sidebar.offsetHeight) + 'px';
 
-  // Footer year
-  yearSpan.innerText = date.getFullYear();
+   // Button for sidebar slide
+   button.addEventListener('click', () => {
+      if (window.innerWidth > 990) {
+         col_1.classList.toggle('sidebar--toggle');
+      } else if (window.innerWidth < 990) {
+         col_1.classList.toggle('sidebar--toggle-inverse');
+      }
+   });
+
+   // Footer year
+   yearSpan.innerText = date.getFullYear();
 })();
