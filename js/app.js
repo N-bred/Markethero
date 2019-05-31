9840ae4a7c39c8275eac599a4445a3f3f3d3e04c
N-bred <thehardyboiz1@gmail.com>
2019-05-30 21:38:30 -0500

    Finish responsive layout

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 (function() {
-   // Var declaration
-   const header = document.querySelector('.header'),
-      sidebar = document.querySelector('.sidebar__logo'),
-      col_1 = document.querySelector('.col-1'),
-      button = document.querySelector('.header__menu-button'),
-      yearSpan = document.querySelector('#footer_year'),
-      date = new Date();
+  // Var declaration
+  const header = document.querySelector('.header'),
+    sidebar = document.querySelector('.sidebar__logo'),
+    col_1 = document.querySelector('.col-1'),
+    button = document.querySelector('.header__menu-button'),
+    yearSpan = document.querySelector('#footer_year'),
+    date = new Date();
 
-   // Height fix to nav
-   header.style.height = String(sidebar.offsetHeight) + 'px';
+  // Height fix to nav
+  header.style.height = String(sidebar.offsetHeight) + 'px';
 
-   // Button for sidebar slide
-   button.addEventListener('click', () => {
+  // Button for sidebar slide
+  button.addEventListener('click', () => {
+    if (window.innerWidth > 990) {
       col_1.classList.toggle('sidebar--toggle');
-   });
+    } else if (window.innerWidth < 990) {
+      col_1.classList.toggle('sidebar--toggle-inverse');
+    }
+  });
 
-   // Footer year
-   yearSpan.innerText = date.getFullYear();
+  // Footer year
+  yearSpan.innerText = date.getFullYear();
 })();
